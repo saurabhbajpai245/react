88479981ae41e6a8f91d27261f61c4aacda93640
saurabh bajpai <saurabhbajpai844@gmail.com>
2024-04-19 10:28:40 +0530

    react-cdn-link

---
+++ App.js
@@ -0,0 +1,3 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+let heading = React.createElement('h1' , {} , 'i am saurabh bajpai');
+root.render(heading);  // renders the element into the DOM
